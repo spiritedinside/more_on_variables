bef82036007553881aeec13fcc18288ef0cc699b
Virginia Walthers <walthvir000@warren.k12.in.us>
2025-08-25 07:54:29 -0400

    finish 8/25

---
+++ more on variables.py
@@ -0,0 +1,11 @@
+school_name = "Warren Central High School"
+math_grade = 76
+science_grade = 87
+english_grade = 101
+all_passing = True
+
+print("school:", school_name)
+print("math grade:", math_grade)
+print("science grade:", science_grade)
+print("english grade:", english_grade)
+print("all passing:", all_passing)
---
+++ variables.py
@@ -0,0 +1,2 @@
+name = "ginny"
+print(name)
