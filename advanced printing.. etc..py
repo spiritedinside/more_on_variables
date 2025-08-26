41920e942a98ec5a746a8a51407fd7465f65c4a5
Virginia Walthers <walthvir000@warren.k12.in.us>
2025-08-26 09:07:10 -0400

    finish 8/26

---
+++ advanced printing.. etc..py
@@ -0,0 +1,25 @@
+# Student bio card
+
+name = "Ginny"
+age = 18
+favorite_food = "steak"
+dream_job = "video game designer"
+hobby = "percussion and playing video games"
+
+# Print using commas
+
+print("name:", name)
+print("age:", age)
+print("favorite food:", favorite_food)
+
+# Print using concatenation
+
+print("dream job:" + " " + dream_job)
+print("hobby:" + " " + hobby)
+
+# Print using f-string
+
+print(f"{name} is {age} years old and wants to be a {dream_job}.")
+print(f"They love eating {favorite_food} and enjoy {hobby}.")
+# TODO: using the f-string format in print so that the output is as follows.  (Value in Name) is (Value in age) years old and want to be a (Value in dream_job).
+# TODO: using the f-string format in print so that the output is as follows.  They love eating (Value in favorite_food) and enjoy (Value in hobby).
